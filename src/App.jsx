13e309e7ab5f8b8eb74e5775d099f95d7b8edc45
David Sommer <sommerdavid1@gmail.com>
2021-03-15 19:30:23 +0100

    base project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>stickertherevolution.ch</h1>
+    </div>
+  );
+}
+
+export default App;
